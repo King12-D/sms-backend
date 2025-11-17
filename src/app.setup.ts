e259dd21860115c39028b7e422f03c5011f72fce
King_Dav <mickyyoung660@gmail.com>
2025-11-17 20:29:00 +0100

    allow frontend access

--- src/app.setup.ts
+++ src/app.setup.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { createServer } from "http";
 import { appRouter } from "./modules/routes";
 
@@ -7,6 +8,15 @@ const server = createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Allow frontend access
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 //Authentication routes
 app.use("/api/v1", appRouter);

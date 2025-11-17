52730ab38070bc70e992f8ecc403c262dba40f7e
King_Dav <mickyyoung660@gmail.com>
2025-11-17 16:17:00 +0100

    add health check route

--- src/app.setup.ts
+++ src/app.setup.ts
@@ -12,4 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 //Authentication routes
 app.use("/api/v1", appRouter);
 
+app.get("/health", (_, res) => {
+  res.status(200).json({ message: "Server is healthy" });
+});
 export default server;
